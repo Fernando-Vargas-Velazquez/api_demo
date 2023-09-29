c97dbe484e40d1b7a2162742ba6e5177e9bfb17f
Fernando Antonio Vargas Velázquez <1722110121@utectulancingo.edu.mx>
2023-09-29 17:59:47 +0000

    Commit

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 import csv
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", 
+    status_code=status.HTTP_201_CREATED,
+    description="Endpoint raiz de la Api Contactos",
+    summary="Endpoint raiz")
 async def root():
+    """
+    #Endpoint raiz
+    ##Status code
+    *201 created
+    """
     return {"message": "Hello World"}
 
-@app.get("/v1/contactos")
+@app.get(
+    "/v1/contactos", 
+    status_code=status.HTTP_201_CREATED,
+    description="Endpoint para listar contactos en la Api Contactos",
+    summary="Endpoint para listar cotactos")
 async def get_contactos():
+    
     # Define la ruta de tu archivo CSV
-    csv_file_path = "contactos.csv"
+    archivo_contactos = "contactos.csv"
 
     # Inicializa una lista para almacenar los datos de contacto
     contactos = []
 
-    # Lee los datos del archivo CSV e imprímelos
-    with open(csv_file_path, mode='r', encoding='utf-8') as file:
+    # Lee los datos del archivo CSV y los impríme
+    with open(archivo_contactos, mode='r', encoding='utf-8') as file:
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
             contactos.append(row)
